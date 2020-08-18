5500f111e46ac39a880dbdb5bba40bc0e1f96b14
Alex <alex.wanigasekara@gmail.com>
2020-08-18 09:30:04 +0530

    first commit

---
+++ src/main/java/com/alex/configserver/ConfigServerRemoteApplication.java
@@ -0,0 +1,15 @@
+package com.alex.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerRemoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerRemoteApplication.class, args);
+    }
+
+}
